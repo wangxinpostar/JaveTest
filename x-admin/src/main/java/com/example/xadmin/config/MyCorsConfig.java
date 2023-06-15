248d6ed1287cc42fd04d065bb8dff226a431bfb3
wangx <1124434059@qq.com>
2023-06-15 09:00:55 +0800

    6/15

---
+++ x-admin/src/main/java/com/example/xadmin/config/MyCorsConfig.java
@@ -0,0 +1,11 @@
+package com.example.xadmin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class MyCrosConfig {
+    @Bean
+    public CorsFilter
+}
